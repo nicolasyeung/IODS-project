55a8a1ece3643c5a1ff5a30f73e9c208229f2bc4
nicolasyeung <nicolas.yeung@live.com>
2023-11-30 12:45:18 +0200

    Assignment 5 data wrangling complete

--- data/create_human.R
+++ data/create_human.R
@@ -25,3 +25,30 @@ human <- inner_join(hd, gii, by = "Country")
 #writing a csv file
 setwd("C:/Users/nicol/Downloads/PHD_302/IODS-project/data/")
 write.csv(human, "human.csv")
+
+#Beginning of Assignment 5 data wrangling
+
+library(readr)
+human2 <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human1.csv")
+
+#task 1 explore the data
+
+dim(human)
+dim(human2)
+
+summary(human)
+summary(human2)
+
+
+#task 2 getting rid of the unneeded columns
+human3 <- subset(human, select = c(Country, Edu2.FM, Labo.FM, Edu.Exp, Life.Exp, GNI, Mat.Mor, Ado.Birth, Parli.F)) 
+
+#task 3 removing NA
+human4 <- na.omit(human3)
+
+#task 4 removing regions
+human5 <- head(human4, -7)
+
+#task 5 re writing the human.csv
+setwd("C:/Users/nicol/Downloads/PHD_302/IODS-project/data/")
+write.csv(human5, "human.csv")
